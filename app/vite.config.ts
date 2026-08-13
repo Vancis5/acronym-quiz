db69793e1b82381af8fbf7795131b33788254ea6
Francis (vancis) <lorzanofrancisevan@gmail.com>
2026-08-13 10:16:15 +0800

    config: set dev server port to 2000

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 2000
+	},
+	preview: {
+		port: 2000
+	},
 	plugins: [
 		sveltekit({
 			compilerOptions: {
